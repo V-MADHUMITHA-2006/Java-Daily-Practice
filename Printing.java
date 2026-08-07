0e6dea8799f25d6ba1df5cd262594708e01cecae
Madhumitha V <madhumithaah06@gmail.com>
2026-08-07 06:59:15 +0530

    Add files via upload

---
+++ Printing.java
@@ -0,0 +1,9 @@
+public class Printing {
+    public static void main(String[] args) {
+        String name = "Manoj";
+        int age = 20;
+        double salary = 10000.50;
+        System.out.printf("Name: %s, Age: %d%n", name, age);
+        System.out.printf("Salary: %.2f%n", salary);
+    }
+}
