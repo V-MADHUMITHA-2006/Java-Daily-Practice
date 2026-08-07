cea8568f73e2e83d2132344cad3d70ab194ee669
Madhumitha V <madhumithaah06@gmail.com>
2026-08-07 18:17:14 +0530

    Add files via upload

---
+++ ConIf.java
@@ -0,0 +1,11 @@
+public class ConIf{
+    public static void main(String[] args){
+        int age = 39;
+        if (age>=18){
+            System.out.println("Adult");
+        }
+        else{
+            System.out.println("Minor");
+        }
+    }
+}
