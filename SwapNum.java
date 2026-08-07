22f6918999877eb893278970af7bc65cf0bbb6f0
Madhumitha V <madhumithaah06@gmail.com>
2026-08-07 07:06:49 +0530

    Add files via upload

---
+++ SwapNum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public static void main(String [] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter first number: ");
+    int num1 = scanner.nextInt();
+    System.out.print("Enter second number: ");
+    int num2 = scanner.nextInt();
+
+    int temp = num1;
+    num1 = num2;
+    num2 = temp;
+
+    System.out.printf("After swapping: First number: %d, Second number: %d%n", num1, num2);
+}
