c434b2db08d4a32d9d53e83a016c698a26384862
Madhumitha V <madhumithaah06@gmail.com>
2026-08-08 19:46:43 +0530

    Add files via upload

---
+++ NestedForExample.java
@@ -0,0 +1,10 @@
+public class NestedForExample {
+    public static void main(String[] args){
+        int i,j;
+        for( i=1;i<=5;i++){
+            for(j=1;j<=10;j++){
+                System.out.println("i="+i+" j="+j);
+            }
+        }
+    }
+}
