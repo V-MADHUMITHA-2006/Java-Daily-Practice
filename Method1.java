3e9e91a6ba203186928dfaa1798e4c1d63624a6b
Madhumitha V <madhumithaah06@gmail.com>
2026-08-11 11:07:35 +0530

    Add files via upload

---
+++ Method1.java
@@ -0,0 +1,16 @@
+ class Method1 {
+    public int addvalue() {
+        int a = 10;
+        int b = 30;
+        int sum = a + b;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Method1 obj = new Method1();
+        int result = obj.addvalue();
+        System.out.println(result);
+    }
+}
+    
+
