2b291c30b4b4d77e96e32202110fb96c40728561
Madhumitha V <madhumithaah06@gmail.com>
2026-08-12 07:17:44 +0530

    Add files via upload

---
+++ Mthod4.java
@@ -0,0 +1,13 @@
+ public class Mthod4 {
+    public int productvalue(int a, int b) {
+        int product = a * b;
+        return product;
+    }
+
+    public static void main(String[] args) {
+        Mthod4 obj = new Mthod4();
+        int result = obj.productvalue(3, 11);
+        System.out.println(result);
+    }
+}
+   
