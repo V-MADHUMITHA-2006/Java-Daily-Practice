5b71d3be7ff80a2ff8e47b17007a5c7a900c5d39
Madhumitha V <madhumithaah06@gmail.com>
2026-08-16 21:46:43 +0530

    Add files via upload

---
+++ righthalfpyramid.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class righthalfpyramid {
+
+    public static void main (String []args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n:");
+        int n = sc.nextInt();
+
+        for(int row = 1; row<=n; row++){
+
+            for(int col=1 ; col<=row; col++){
+
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+        
+
+    }
+    
+    
+}
